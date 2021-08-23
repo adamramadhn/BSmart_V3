7768b3d7860c3b9ad1ae01aac7bb833c2e6a0f1d
cendekialnazalia <cendekialnazalia@gmail.com>
2021-08-23 16:56:41 +0700

    fix app realase and add enkripsi sharedpreference

--- app/src/main/java/com/bpkp/bsmartapp/login/PrefHelper.kt
+++ app/src/main/java/com/bpkp/bsmartapp/login/PrefHelper.kt
@@ -2,6 +2,12 @@ package com.bpkp.bsmartapp.login
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.securepreferences.SecurePreferences
 
 class PrefHelper(context: Context) {
 
@@ -9,33 +15,33 @@ class PrefHelper(context: Context) {
     private val PREFS_NAME = "sharedpref12345"
 
     //CONSTRUCTOR
-    private val sharedPref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+//    private val sharedPref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    private var sharedPref =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val spec = KeyGenParameterSpec.Builder(
+                MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build()
+            val masterKey = MasterKey.Builder(context)
+                .setKeyGenParameterSpec(spec)
+                .build()
+            EncryptedSharedPreferences
+                .create(
+                    context,
+                    PREFS_NAME,
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+        } else {
+            SecurePreferences(context, "bsmart", PREFS_NAME)
+        }
     private val editor: SharedPreferences.Editor = sharedPref.edit()
-    //    private var sharedPref: SharedPreferences =
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            val spec = KeyGenParameterSpec.Builder(
-//                MasterKey.DEFAULT_MASTER_KEY_ALIAS,
-//                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-//            )
-//                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-//                .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
-//                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-//                .build()
-//            val masterKey = MasterKey.Builder(context)
-//                .setKeyGenParameterSpec(spec)
-//                .build()
-//            EncryptedSharedPreferences
-//                .create(
-//                    context,
-//                    PREFS_NAME,
-//                    masterKey,
-//                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-//                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-//                )
-//        }
-//        else {
-//            SecurePreferences(context, "bsmart", PREFS_NAME)
-//        }
+
 
 
     //SETTER
